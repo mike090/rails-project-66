fa3a9995dac00f77b4201bbb891fcb0037ed5633
mike09 <mike09@mail.ru>
2022-08-24 10:11:01 +0300

    add bootstrap

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -7,6 +7,8 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import '../js/bootstrap_files.js'
+import '../stylesheets/application'
 
 Rails.start()
 Turbolinks.start()
