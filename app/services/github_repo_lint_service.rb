20d5f3c8deb65c9dd619d8492b34befc435f2188
mike09 <mike09@mail.ru>
2022-09-08 11:02:55 +0300

    fix lint service to return relative blob path

--- app/services/github_repo_lint_service.rb
+++ app/services/github_repo_lint_service.rb
@@ -9,12 +9,10 @@ class GithubRepoLintService
       clone_path = FileUtils.mkpath(Rails.root.join("tmp/repos/#{SecureRandom.urlsafe_base64}")).first
       git = Git.clone clone_url, clone_path
       sha = git.object('HEAD').sha[0..6]
-      blob_url = "#{repo_info.html_url}/blob/#{sha}"
       check_results = languages.map do |language|
         check_result = CodeChecker.check(clone_path, language)
         check_result[:errors]&.map! do |file|
-          rel_path = Pathname.new(file.delete(:path)).relative_path_from(Pathname.new(clone_path))
-          file[:uri] = "#{blob_url}/#{rel_path}"
+          file[:blob_path] = Pathname.new(file.delete(:path)).relative_path_from(Pathname.new(clone_path)).to_s
           file
         end
         check_result
