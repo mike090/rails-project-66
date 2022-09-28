545cc19222646482f15b2f838595711d1a6c4fb9
mike09 <mike09@mail.ru>
2022-09-28 14:20:22 +0300

    remove unused params from GithubAdapter#set_repo_hook

--- app/services/github_adapter.rb
+++ app/services/github_adapter.rb
@@ -18,19 +18,17 @@ class GithubAdapter
       Octokit.client.languages(github_id).to_h.keys
     end
 
-    def set_repo_hook(github_repo_id, payload_url, access_token, *events)
+    def set_repo_hook(github_repo_id, payload_url, access_token)
       octokit_client = Octokit::Client.new access_token: access_token
       return if octokit_client.hooks(github_repo_id).any? { |hook| hook.config.url == payload_url }
 
-      options = events.difference([:push]).empty? ? {} : { events: events, active: true }
       octokit_client.create_hook(
         github_repo_id,
         'web',
         {
           url: payload_url,
           content_type: 'json'
-        },
-        options
+        }
       )
     end
   end
