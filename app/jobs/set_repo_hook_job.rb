a2e8c168f3b15b98a4f9e1cae4d163934ee95473
mike09 <mike09@mail.ru>
2022-10-08 08:54:52 +0300

    remove events param from set hook job

--- app/jobs/set_repo_hook_job.rb
+++ app/jobs/set_repo_hook_job.rb
@@ -3,8 +3,8 @@
 class SetRepoHookJob < ApplicationJob
   queue_as :default
 
-  def perform(repo_id, payload_url, *events)
-    ApplicationContainer['remote_service'].set_repo_hook repo_id, payload_url, *events
+  def perform(repo_id, payload_url)
+    ApplicationContainer['remote_service'].set_repo_hook repo_id, payload_url
     Rails.logger.info "Webhook for repo id: #{repo_id} successfly setted"
   rescue StandardError => e
     Rails.logger.error "Repo id: #{repo_id} webhook setting error: #{e.class} - #{e.message}"
